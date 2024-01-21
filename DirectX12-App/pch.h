a4b184e861ac03e36cf6749ae40cf1fdb5bc96d2
CoconutCoder1 <93152788+CoconutCoder1@users.noreply.github.com>
2024-01-21 08:35:50 +0100

    Upload base project

---
+++ DirectX12-App/pch.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <memory>
+#include <thread>
+#include <utility>
+#include <stdexcept>
+#include <format>
+#include <fstream>
+#include <cassert>
+
+// Data structures
+#include <string>
+#include <stack>
+#include <deque>
+#include <array>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+
+// Windows API
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+// DirectX
+#include <d3d11.h>
+#include <dxgi.h>
+
+#define PI 3.14159265359
+#define DEG2RAD (PI / 180.f)
+#define RAD2DEG (180.f / PI)
